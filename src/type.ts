de50b084f3f953cce3bbe8fcb964019437c97677
WalterBright <ssmbuild@outlook.com>
2024-02-02 02:37:36 +0800

    update

--- src/type.ts
+++ src/type.ts
@@ -1,20 +1,23 @@
 interface Task {
-  danger: 1 | 2 | 3
+  danger: 0 | 1 | 2 | 3
   sp: number
-  coin: number
+  packCount: number
 }
 
 interface Stage {
-  title: string
+  stage: number
+  sp: number // 当前关卡所有sp
   spFinish: number //完成当前的sp，等于下一关解锁需要的sp
   tasks: Task[]
 }
 
 interface Chapter {
   title: string
-  stages: Stage[]
+  chapter: number
+  stages: Stage[] // 本章每个阶段
+  coinPerPack: number //本章每个包的福币数
 }
 
-interface DS {
+interface DSData {
   chapters: Chapter[]
 }
---
+++ src/views/eventData.ts
@@ -0,0 +1,87 @@
+const eventData: DSData = {
+  chapters: [
+    {
+      chapter: 1,
+      title: '暗流涌动',
+      coinPerPack: 48,
+      stages: [
+        {
+          stage: 1,
+          sp: 888,
+          spFinish: 888,
+          tasks: [
+            {
+              danger: 0,
+              sp: 888,
+              packCount: 8,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      chapter: 2,
+      title: '如履薄冰',
+      coinPerPack: 88,
+      stages: [
+        {
+          stage: 1,
+          sp: 7888,
+          spFinish: 16776,
+          tasks: [
+            { danger: 3, sp: 4063, packCount: 7 },
+            { danger: 3, sp: 4277, packCount: 8 },
+            { danger: 3, sp: 4705, packCount: 9 },
+          ],
+        },
+        {
+          stage: 2,
+          sp: 7888,
+          spFinish: 24664,
+          tasks: [
+            { danger: 2, sp: 1884, packCount: 6 },
+            { danger: 2, sp: 1983, packCount: 7 },
+            { danger: 2, sp: 2181, packCount: 8 },
+            { danger: 3, sp: 4063, packCount: 14 },
+            { danger: 3, sp: 4277, packCount: 15 },
+            { danger: 3, sp: 4705, packCount: 16 },
+          ],
+        },
+        {
+          stage: 3,
+          sp: 9888,
+          spFinish: 34552,
+          tasks: [
+            { danger: 1, sp: 812, packCount: 3 },
+            { danger: 1, sp: 855, packCount: 3 },
+            { danger: 1, sp: 941, packCount: 3 },
+            { danger: 2, sp: 1884, packCount: 7 },
+            { danger: 2, sp: 1983, packCount: 8 },
+            { danger: 2, sp: 2181, packCount: 9 },
+            { danger: 3, sp: 4063, packCount: 15 },
+            { danger: 3, sp: 4277, packCount: 16 },
+            { danger: 3, sp: 4705, packCount: 17 },
+          ],
+        },
+        {
+          stage: 3,
+          sp: 12570,
+          spFinish: 47122,
+          tasks: [
+            { danger: 1, sp: 812, packCount: 4 },
+            { danger: 1, sp: 855, packCount: 4 },
+            { danger: 1, sp: 941, packCount: 4 },
+            { danger: 2, sp: 1884, packCount: 8 },
+            { danger: 2, sp: 1983, packCount: 9 },
+            { danger: 2, sp: 2181, packCount: 10 },
+            { danger: 3, sp: 4063, packCount: 20 },
+            { danger: 3, sp: 4277, packCount: 22 },
+            { danger: 3, sp: 4705, packCount: 24 },
+          ],
+        },
+      ],
+    },
+  ],
+}
+
+export default eventData
