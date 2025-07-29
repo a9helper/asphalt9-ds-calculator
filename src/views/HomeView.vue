03e3a6c46f707c459b60f32436d476f814584746
WalterBright <ssmbuild@outlook.com>
2025-07-29 23:25:11 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@ import useFormStore from './form.store'
 
 const eventData = ref<DSData>({
   _id: 'gl-tartarus',
-  server: 'al',
+  server: 'gl',
   chapters: [],
 })
 
@@ -26,7 +26,7 @@ const getEventData = async () => {
   // if (debug) {
   //   res2 = [localData] as any
   // }
-  const target = res2.find((item) => item._id === 'gl-tartarus')
+  const target = res2.find((item) => item._id === 'gl-emperium')
   if (target) {
     for (let chapter of target.chapters) {
       for (let stage of chapter.stages) {
